3998dc835cd77f1f4d0d3199557a7bd7e8752ade
Fardin-Mohammed <39059@ma-web.nl>
2025-05-13 12:56:05 +0200

    03 is af

--- solutions/subproject/Program.cs
+++ solutions/subproject/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-int mijnInt = 42;
-string mijnString = "Mario";
-bool mijnBool = false;
-float mijnKomma = 3.14f;
